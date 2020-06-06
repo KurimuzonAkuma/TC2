0ec7a82a68d7e42b0bd8b0c1fd34b27db6a5ea34
BuildTools <unconfigured@null.spigotmc.org>
2020-06-06 12:11:54 +0300

    Changing colors

--- MAIN.C
+++ MAIN.C
@@ -4,8 +4,8 @@
 #include <conio.h>
 int main() {
     window_t *w1, *w2;
-      w1 = window_create ("Window 1", 3, 23, 3, 30, WHITE | BGBLUE, DUAL_BORDER);
-      w2 = window_create ("Window 2", 5, 12, 20, 70, WHITE | BGRED, SINGLE_BORDER);
+      w1 = window_create ("Window 1", 3, 17, 3, 30, GREEN | BGBLACK, DUAL_BORDER);
+      w2 = window_create ("Window 2", 5, 20, 25, 70, RED | BGWHITE, SINGLE_BORDER);
     	clear_screen();
     	window_open (w1);
     	window_open (w2);
