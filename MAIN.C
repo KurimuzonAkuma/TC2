eb3a46f6137cf740c82c5bcef1079041bd0776e9
KurimuzonAkuma <arturgayauskas@gmail.com>
2020-06-06 12:25:26 +0300

    1

--- MAIN.C
+++ MAIN.C
@@ -4,7 +4,7 @@
 #include <conio.h>
 int main() {
     window_t *w1, *w2;
-      w1 = window_create ("Window 1", 3, 17, 3, 30, GREEN | BGBLACK, DUAL_BORDER);
+      w1 = window_create ("Black window", 3, 17, 3, 30, GREEN | BGBLACK, DUAL_BORDER);
       w2 = window_create ("Window 2", 5, 20, 25, 70, RED | BGWHITE, SINGLE_BORDER);
     	clear_screen();
     	window_open (w1);
